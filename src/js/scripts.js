0615e00b54795d5cd8503f97a6fd457aac20b566
mizza-cz <mizza.cz666@gmail.com>
2025-06-10 10:50:41 +0200

    start

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+//=include ../../node_modules/vanilla-lazyload/dist/lazyload.js
+//=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
+//=include ../../node_modules/slick-carousel/slick/slick.js
+//=include ../../node_modules/select2/dist/js/select2.min.js
+//=include ../../node_modules/fslightbox/index.js
+document.addEventListener("DOMContentLoaded", function() {
+//=include ../components/**/*.js
+//=include custom/*.js
+});
